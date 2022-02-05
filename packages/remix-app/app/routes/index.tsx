51914ba44c348f2f55e3c524fe3e6ce9e766b4e0
Herve07h22 <contact@storeed.me>
2022-02-05 15:50:25 +0100

    added email form

--- packages/remix-app/app/routes/index.tsx
+++ packages/remix-app/app/routes/index.tsx
@@ -1,11 +1,4 @@
-import { App } from "@sugggest/core/App";
 import { Hero } from "~/components/sections/Hero";
-
-export async function loader() {
-  const result = await App.getAToken({ email: "herve@camilab.co" });
-  console.log("result: ", result);
-  return { status: "success" };
-}
 
 export default function IndexRoute() {
   return (
---
+++ packages/remix-app/app/routes/signup.tsx
@@ -0,0 +1,23 @@
+import { Container, VStack } from "@chakra-ui/react";
+import { ActionFunction, Outlet, redirect } from "remix";
+import { App } from "@sugggest/core/App";
+
+export const action: ActionFunction = async ({ request }) => {
+    const body = await request.formData();
+    const email = body.get("title")?.toString()
+    const result = await App.getAToken({ email: email || "" });
+    if (result.status==="success") return redirect("/signup/sent");
+    return redirect("/signup/error")
+  }
+
+  
+export default function SignupRoute () {
+
+    return (
+        <Container maxW="2xl">
+        <VStack spacing={8} align="stretch" mb="5rem">
+            <Outlet />
+        </VStack>
+        </Container>
+    )
+}
